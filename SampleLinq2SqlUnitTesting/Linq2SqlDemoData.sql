5dfa2c79dc631a1b81543655b423a9339c983d6e
Frank DeCaire <frank@decaire.net>
2019-04-19 19:05:11 +0000

    updated sample data query

--- SampleLinq2SqlUnitTesting/Linq2SqlDemoData.sql
+++ SampleLinq2SqlUnitTesting/Linq2SqlDemoData.sql
@@ -123,9 +123,14 @@ CREATE TABLE [dbo].[Department](
 GO
 
 CREATE TABLE [dbo].[Employee](
+	[ID][int] IDENTITY(1,1) NOT NULL,
 	[FirstName] [varchar](50) NOT NULL,
 	[LastName] [varchar](50) NOT NULL,
 	[Department] [int] NOT NULL
+	CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO
