dd18896ac17780898a515973d93b2c98c76afa01
Frank DeCaire <frank@decaire.net>
2019-07-08 22:41:32 +0000

    Added tests for new UnitTestHelpers features

--- SampleLinq2SqlUnitTesting.Tests/BusinessClassTests.cs
+++ SampleLinq2SqlUnitTesting.Tests/BusinessClassTests.cs
@@ -35,12 +35,35 @@ namespace SampleLinq2SqlUnitTesting.Tests
         [TestMethod]
         public void StoredProcedureTest()
         {
-            UnitTestHelpers.ExecuteSQLCode("SampleLinq2SqlUnitTesting.Tests.TestData.PopulateDepartmentTable.sql", "Linq2SqlDemoData");
+            UnitTestHelpers.ExecuteSQLCodeFromFile("SampleLinq2SqlUnitTesting.Tests.TestData.PopulateDepartmentTable.sql", "Linq2SqlDemoData");
 
             var businessClass = new BusinessClass();
             var result = businessClass.GetAllDepartments();
 
             Assert.AreEqual(3, result.Count);
+        }
+
+        [TestMethod]
+        public void ExecuteDirectSqlStatement()
+        {
+            UnitTestHelpers.ExecuteSQLQuery("INSERT INTO Department (Name) values ('IT')", "Linq2SqlDemoData");
+            UnitTestHelpers.ExecuteSQLQuery("INSERT INTO Department (Name) values ('Accounting')", "Linq2SqlDemoData");
+
+            var businessClass = new BusinessClass();
+            var result = businessClass.GetAllDepartments();
+
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [TestMethod]
+        public void ReadDepartmentList()
+        {
+            UnitTestHelpers.ExecuteSQLCodeFromFile("SampleLinq2SqlUnitTesting.Tests.TestData.PopulateDepartmentTable.sql", "Linq2SqlDemoData");
+
+            var listOfDepartments = UnitTestHelpers.ReadDataFromTable<TestDTO.Department>("department", "Linq2SqlDemoData");
+
+            Assert.AreEqual(3, listOfDepartments.Count);
+            Assert.AreEqual("Sales", listOfDepartments[0].Name);
         }
     }
 }
---
+++ SampleLinq2SqlUnitTesting.Tests/TestDTO/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleLinq2SqlUnitTesting.Tests.TestDTO
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
